8263e6a2468d93956064981f4221bc60305149bc
gahyuun <pgh123166@naver.com>
2025-01-15 02:16:46 +0900

    chore: LoginRequest idToken NotBlank 추가 (#11)

--- src/main/java/com/acon/server/member/api/request/LoginRequest.java
+++ src/main/java/com/acon/server/member/api/request/LoginRequest.java
@@ -1,12 +1,14 @@
 package com.acon.server.member.api.request;
 
 import com.acon.server.member.domain.enums.SocialType;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record LoginRequest(
         @NotNull
         SocialType socialType,
         @NotNull
+        @NotBlank
         String idToken
 ) {
 
