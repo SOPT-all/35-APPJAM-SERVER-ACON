5981607ad1e33572f0d1dbc82fb214c2d620fc0e
ckkim817 <ckkim817@konkuk.ac.kr>
2025-01-16 01:39:57 +0900

    feat: SpotOption 도메인 엔티티 작성 (#15)

---
+++ src/main/java/com/acon/server/spot/domain/entity/SpotOption.java
@@ -0,0 +1,21 @@
+package com.acon.server.spot.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class SpotOption {
+
+    private final Long id;
+    private final Long spotId;
+    private final Long optionId;
+
+    @Builder
+    public SpotOption(Long id, Long spotId, Long optionId) {
+        this.id = id;
+        this.spotId = spotId;
+        this.optionId = optionId;
+    }
+}
