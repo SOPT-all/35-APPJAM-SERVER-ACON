49b04a454c5e6b0edc36de51294b8e15fb44e9b0
gahyuun <pgh123166@naver.com>
2025-01-15 21:19:42 +0900

    feat: 온보딩 request body 유효성 검사 진행 (#14)

--- src/main/java/com/acon/server/member/api/controller/PreferenceController.java
+++ src/main/java/com/acon/server/member/api/controller/PreferenceController.java
@@ -2,6 +2,11 @@ package com.acon.server.member.api.controller;
 
 import com.acon.server.member.api.request.PreferenceRequest;
 import com.acon.server.member.application.service.PreferenceService;
+import com.acon.server.member.domain.enums.Cuisine;
+import com.acon.server.member.domain.enums.DislikeFood;
+import com.acon.server.member.domain.enums.FavoriteSpot;
+import com.acon.server.member.domain.enums.SpotStyle;
+import com.acon.server.member.domain.enums.SpotType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +22,15 @@ public class PreferenceController {
 
     private final PreferenceService preferenceService;
 
-    @PostMapping()
+    @PostMapping
     public ResponseEntity<Void> postPreference(@Valid @RequestBody final PreferenceRequest request) {
-        // 토큰으로 memberId 가져오기
-        preferenceService.createPreference(request, 123L);
+        // ToDo 토큰으로 memberId 가져오기
+        preferenceService.createPreference(request, 1L);
+        request.dislikeFoodList().forEach(dislikeFood -> DislikeFood.fromValue(String.valueOf(dislikeFood)));
+        request.favoriteCuisineRank().forEach(cuisine -> Cuisine.fromValue(String.valueOf(cuisine)));
+        SpotType.fromValue(String.valueOf(request.favoriteSpotType()));
+        SpotStyle.fromValue(String.valueOf(request.favoriteSpotStyle()));
+        request.favoriteSpotRank().forEach(favoriteSpot -> FavoriteSpot.fromValue(String.valueOf(favoriteSpot)));
         return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/acon/server/member/api/request/PreferenceRequest.java
+++ src/main/java/com/acon/server/member/api/request/PreferenceRequest.java
@@ -5,13 +5,17 @@ import com.acon.server.member.domain.enums.DislikeFood;
 import com.acon.server.member.domain.enums.FavoriteSpot;
 import com.acon.server.member.domain.enums.SpotStyle;
 import com.acon.server.member.domain.enums.SpotType;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record PreferenceRequest(
+        @NotNull
         List<DislikeFood> dislikeFoodList,
+        @NotNull
         List<Cuisine> favoriteCuisineRank,
         SpotType favoriteSpotType,
         SpotStyle favoriteSpotStyle,
+        @NotNull
         List<FavoriteSpot> favoriteSpotRank
 ) {
 
