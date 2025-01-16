ffa27bda6eb6643b85ce469e23bdbf7634211bf9
gahyuun <pgh123166@naver.com>
2025-01-16 22:20:27 +0900

    feat: recent guided spot repository 생성 (#22)

---
+++ src/main/java/com/acon/server/member/infra/repository/RecentGuidedSpotRepository.java
@@ -0,0 +1,8 @@
+package com.acon.server.member.infra.repository;
+
+import com.acon.server.member.infra.entity.RecentGuidedSpotEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecentGuidedSpotRepository extends JpaRepository<RecentGuidedSpotEntity, Long> {
+
+}
