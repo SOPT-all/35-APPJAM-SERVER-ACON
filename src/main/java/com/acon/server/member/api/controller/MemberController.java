bcae3fe3f90650ab32027a4df7ed09edaa3e62d3
gahyuun <pgh123166@naver.com>
2025-01-16 22:57:49 +0900

    chore: private final 추가 (#22)

--- src/main/java/com/acon/server/member/api/controller/MemberController.java
+++ src/main/java/com/acon/server/member/api/controller/MemberController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class MemberController {
 
-    MemberService memberService;
+    private final MemberService memberService;
 
     @PostMapping("/member/guided-spot")
     public ResponseEntity<Void> postGuidedSpot(@Valid @RequestBody final SpotRequest request) {
