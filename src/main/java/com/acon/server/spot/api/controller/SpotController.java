f57e19052ffd2d7e009ca4905b95b26debe42997
gahyuun <pgh123166@naver.com>
2025-01-17 04:28:23 +0900

    chore: validated message 설정 (#20)

--- src/main/java/com/acon/server/spot/api/controller/SpotController.java
+++ src/main/java/com/acon/server/spot/api/controller/SpotController.java
@@ -2,6 +2,7 @@ package com.acon.server.spot.api.controller;
 
 import com.acon.server.spot.api.response.MenuListResponse;
 import com.acon.server.spot.application.service.SpotService;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -10,18 +11,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Validated
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
-@RestController
 public class SpotController {
 
     private final SpotService spotService;
 
     @GetMapping("/spot/{spotId}/menus")
     public ResponseEntity<MenuListResponse> getMenus(
-            @PathVariable(name = "spotId") Long spotId
+            @Positive(message = "spotId는 양수여야 합니다.")
+            @Validated @PathVariable(name = "spotId") Long spotId
     ) {
-        return ResponseEntity.ok(spotService.fetchMenus(spotId));
+        return ResponseEntity.ok(
+                spotService.fetchMenus(spotId)
+        );
     }
 }
