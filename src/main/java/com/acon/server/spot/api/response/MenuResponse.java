3752fe2fa9dc61a72fc6c0545062edc3b5dddbb7
gahyuun <pgh123166@naver.com>
2025-01-17 04:51:53 +0900

    refactor: MenuResponse NonNull 처리 (#20)

--- src/main/java/com/acon/server/spot/api/response/MenuResponse.java
+++ src/main/java/com/acon/server/spot/api/response/MenuResponse.java
@@ -1,12 +1,19 @@
 package com.acon.server.spot.api.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Builder;
+import lombok.NonNull;
 
 @Builder
+@JsonInclude(Include.NON_NULL)
 public record MenuResponse(
+        @NonNull
         Long id,
+        @NonNull
         String name,
-        int price,
+        @NonNull
+        Integer price,
         String image
 ) {
 
