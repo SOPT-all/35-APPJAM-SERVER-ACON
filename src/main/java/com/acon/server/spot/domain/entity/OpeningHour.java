d9c81b2d349c67da7eeca1a11ca6b6fc57675952
ckkim817 <ckkim817@konkuk.ac.kr>
2025-01-19 20:57:18 +0900

    fix: OpeningHour 도메인 엔티티 오류 수정 (#24)

--- src/main/java/com/acon/server/spot/domain/entity/OpeningHour.java
+++ src/main/java/com/acon/server/spot/domain/entity/OpeningHour.java
@@ -1,7 +1,7 @@
 package com.acon.server.spot.domain.entity;
 
 import java.time.DayOfWeek;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -14,16 +14,16 @@ public class OpeningHour {
     private final Long spotId;
 
     private DayOfWeek dayOfWeek;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
     @Builder
     public OpeningHour(
             Long id,
             Long spotId,
             DayOfWeek dayOfWeek,
-            LocalDateTime startTime,
-            LocalDateTime endTime
+            LocalTime startTime,
+            LocalTime endTime
     ) {
         this.id = id;
         this.spotId = spotId;
