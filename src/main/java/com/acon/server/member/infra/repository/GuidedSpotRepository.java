726fc1a118901d2986c9c4d964e8fbe48ce1c821
ckkim817 <ckkim817@konkuk.ac.kr>
2025-01-21 22:51:34 +0900

    feat: 추천 검색어 조회 JPA 메서드 작성 (#37)

--- src/main/java/com/acon/server/member/infra/repository/GuidedSpotRepository.java
+++ src/main/java/com/acon/server/member/infra/repository/GuidedSpotRepository.java
@@ -1,8 +1,10 @@
 package com.acon.server.member.infra.repository;
 
 import com.acon.server.member.infra.entity.GuidedSpotEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GuidedSpotRepository extends JpaRepository<GuidedSpotEntity, Long> {
 
+    Optional<GuidedSpotEntity> findTopByMemberIdOrderByUpdatedAtDesc(Long memberId);
 }
