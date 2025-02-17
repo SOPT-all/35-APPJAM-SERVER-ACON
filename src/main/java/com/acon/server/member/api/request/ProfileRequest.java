5fb5d53ad754ec9413ab352145880f5ed726c249
ckkim817 <ckkim817@konkuk.ac.kr>
2025-02-18 05:29:21 +0900

    fix: profileImage Valid 어노테이션 적용 범위 수정 (#78)

--- src/main/java/com/acon/server/member/api/request/ProfileRequest.java
+++ src/main/java/com/acon/server/member/api/request/ProfileRequest.java
@@ -3,10 +3,11 @@ package com.acon.server.member.api.request;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @JsonInclude(Include.NON_NULL)
 public record ProfileRequest(
-        @NotBlank(message = "profileImage 공백일 수 없습니다.")
+        @NotNull(message = "profileImage는 필수입니다.")
         String profileImage,
         @NotBlank(message = "nickname은 공백일 수 없습니다.")
         String nickname,
